b63a96d770dbf445f7baf904551e297c8681e8d2
samwelmagaiwa <samwelmagaiwa229@gmail.com>
2026-01-22 16:29:48 +0300

    deployment batch

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
     {
         //
          if (app()->environment('production')) {
-             URL::forceScheme('https')
+             URL::forceScheme('https');
          }
     }
 
